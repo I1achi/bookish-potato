04a06b40358608110b4c4b4ab8597971d3f3b1af
I1achi <johnthanwickson@proton.me>
2026-04-17 15:59:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,10 +83,10 @@ def create_overlay(data):
     # ⚠️ Adjust coordinates EXACTLY as per PDF
 
     # First Name (boxes)
-    draw_boxes(can, data["first_name"], 195, 593)
+    draw_boxes(can, data["first_name"], 195, 590)
 
     # Middle Name
-    draw_boxes(can, data["middle_name"], 195, 583)
+    draw_boxes(can, data["middle_name"], 195, 577)
 
     # Last Name
     draw_boxes(can, data["last_name"], 195, 603)
